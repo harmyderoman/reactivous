cf75c472713aaa226fa7c4b01c43825fa949014b
harmyderoman <romangarmider@gmail.com>
2023-04-21 14:20:30 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// @ts-ignore
+import dts from 'vite-plugin-dts'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import * as packageJson from './package.json'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ['src/lib/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'lib/index.ts'),
+      name: 'Reactivous',
+      formats: ['es', 'umd'],
+      fileName: (format) => `reactivous.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}))
